f5039240d8d8952a83a19a9e618a57b361bfe46a
Arizaaaa <74736573+Arizaaaa@users.noreply.github.com>
2022-09-30 20:19:20 +0200

    PHP (M07)
    otro mas

---
+++ M07 Desarrollo web en entorno servidor/Productos/datosRegistro.php
@@ -0,0 +1,92 @@
+<?php
+
+$nomUsuario = $_GET['nomUsuario'];
+$pass = $_GET['pass'];
+$nombre = $_GET['nombre'];
+$apellidos = $_GET['apellidos'];
+$email = $_GET['email'];
+
+$servidor="localhost";
+$usuario="root";
+$password="usbw";
+$bd="prod";
+
+$con=mysqli_connect($servidor,$usuario,$password,$bd);
+
+if($con){
+    mysqli_set_charset($con,"utf8");
+
+    $sql="INSERT INTO `user`(`id`, `nomUsuario`, `pass`, `admin`, `nombre`, `apellidos`, `email`) 
+    VALUES (NULL,'$nomUsuario','$pass', 0, '$nombre', '$apellidos', '$email')";
+    
+    $consulta=mysqli_query($con,$sql);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <title>Vista de productos</title>
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Mercadowna</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="InsertarProductos.html">Introducir</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="ActualizarProductos.html">Actualizar</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="BuscarProductos.html">Buscar</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="BorrarProductos.html">Borrar</a>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Buscar</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    <div>
+      <table class="table table-dark table-borderless">
+    <?php
+        echo "<td>NOMBRE</td>";
+        echo "<td>DESCRIPCIÓN</td>";
+        echo "<td>CANTIDAD</td>";
+        echo "<td>PRECIO</td>";
+        echo "<tr>";
+        echo "<td>".$nombre."</td>";
+        echo "<td>".$descripcion."</td>";
+        echo "<td>".$cantidad."</td>";
+        echo "<td>".$precio."</td>";
+        echo "</tr>";
+    ?>
+    </table>
+    <button class="btn btn-outline-primary"><a href="InsertarProductos.html">ATRÁS</a></button>
+    </div>
+</body>
+</html>
+
+
+<?php
+}
+?>
--- M07 Desarrollo web en entorno servidor/Productos/prod.sql
+++ M07 Desarrollo web en entorno servidor/Productos/prod.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 23-09-2022 a las 16:48:17
+-- Tiempo de generación: 30-09-2022 a las 18:11:37
 -- Versión del servidor: 5.7.36
 -- Versión de PHP: 8.1.3
 
@@ -47,6 +47,29 @@ INSERT INTO `prod` (`id`, `nombre`, `descripcion`, `cantidad`, `precio`) VALUES
 (5, '1', '1', 1, 1),
 (6, '1', '1', 1, 1);
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `nomUsuario` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `pass` varchar(150) COLLATE utf8_spanish2_ci NOT NULL,
+  `admin` int(11) NOT NULL,
+  `nombre` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `apellidos` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id`, `nomUsuario`, `pass`, `admin`, `nombre`, `apellidos`, `email`) VALUES
+(1, 'ToniGuapo69', 'qwe', 0, 'Toni', 'Chiquero', '');
+
 --
 -- Índices para tablas volcadas
 --
@@ -58,6 +81,12 @@ ALTER TABLE `prod`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -66,6 +95,12 @@ ALTER TABLE `prod`
 --
 ALTER TABLE `prod`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
