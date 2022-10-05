a4107631a25767ab5389a6d0ead9548b10c8278b
Arizaaaa <74736573+Arizaaaa@users.noreply.github.com>
2022-10-05 20:21:18 +0200

    TS (M06)

---
+++ M06 Desarrollo web en entorno cliente/Angular/Practica/src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+
+interface Character {
+  name: string;
+  strength: number;
+  agility: number;
+  intelligence: number;
+  life: number;
+  editable?: boolean;
+  button: string;
+  print?: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  serverCharacters: Character[] = [];
+
+  constructor() {
+    // Ejemplo de respuesta de un servidor en formato JSON
+    const serverJson = `[
+      {"name": "Jugger", "strength": 18, "agility": 12, "intelligence": 6, "life": 30, "editable": false, "button": "Edit" },
+      {"name": "Pelegrin", "strength": 20, "agility": 8, "intelligence": 6, "life": 40, "editable": false, "button": "Edit" },
+      {"name": "Dorthak", "strength": 12, "agility": 18, "intelligence": 10, "life": 16, "editable": false, "button": "Edit" } ,
+      {"name": "Kharak", "strength": 8, "agility": 20, "intelligence": 12, "life": 14, "editable": false, "button": "Edit" },
+      {"name": "Perz", "strength": 10, "agility": 6, "intelligence": 20, "life": 10, "editable": false, "button": "Edit" }
+    ]`;
+
+    // Parseamos la información y la convertimos directamente en un array de "Character"
+    this.serverCharacters = JSON.parse(serverJson);
+  }
+
+  edit(character:Character) {
+
+    console.log(character.editable);
+
+    if (character.editable != false) {
+      character.editable = false;
+      character.button = "Edit";
+    } else if (character.editable == false){
+      character.editable = true;
+      character.button = "Save";
+    }
+
+  }
+
+
+
+}
