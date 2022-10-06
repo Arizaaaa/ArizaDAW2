38e6b371adddbfa0cae4c2b78c26971ca085a622
Arizaaaa <74736573+Arizaaaa@users.noreply.github.com>
2022-10-06 18:05:58 +0200

    JavaDoc (M08)

---
+++ M08 Despliegue de aplicaciones web/JavaDoc/JavaDoc/src/Coso1.java
@@ -0,0 +1,31 @@
+
+public class Coso1 {
+	
+	private int variable1;
+	private String variable2;
+	private Boolean variable3;
+	
+	public Coso1() {
+		
+		this.variable1 = 5;
+		this.variable2 = "Adaji";
+		this.variable3 = true;
+		
+	}
+
+	public int getVariable1() {	return variable1; }
+	public void setVariable1(int variable1) { this.variable1 = variable1; }
+
+	public String getVariable2() { return variable2; }
+	public void setVariable2(String variable2) { this.variable2 = variable2; }
+
+	public Boolean getVariable3() { return variable3; }
+	public void setVariable3(Boolean variable3) { this.variable3 = variable3; }
+	
+	public static void losCososHacenCosas() {
+		
+		System.out.println();
+		
+	}
+	
+}
