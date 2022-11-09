f54c76980300637e994153e7b68b0abb2c575209
Arizaaaa <74736573+Arizaaaa@users.noreply.github.com>
2022-11-09 19:49:29 +0100

    Mierda acabada del todo (M06 - M09)

--- M06 Desarrollo web en entorno cliente/Angular/PracticaCalendario/src/app/app.component.ts
+++ M06 Desarrollo web en entorno cliente/Angular/PracticaCalendario/src/app/app.component.ts
@@ -4,12 +4,13 @@ interface calendario{
   nombre:string;
   semana:string[];
   dias:any[];
+  festivos:any[]
   diaInicial:number;
   diaFinal:number;
-  nacional:any;
-  regional:any;
-  local:any;
-  centro:any;
+  nacional:any[];
+  regional:any[];
+  local:any[];
+  centro:any[];
 }
 
 @Component({
@@ -25,32 +26,47 @@ export class AppComponent{
   colorLocal:string = "rgb(202, 102, 152)";
   colorCentro:string = "rgb(211, 208, 9)";
 
-  septiembre:Array <calendario> = [
-    {nombre:"Septiembre", semana:['L', 'M', 'X', 'J', 'V', 'S','D'], dias:[], diaInicial:3, diaFinal:30, nacional:0, regional:11, local:29, centro:0},
-  ]
-
-  octubre:Array <calendario> = [
-    {nombre:"Octubre", semana:['L', 'M', 'X', 'J', 'V', 'S','D'], dias:[], diaInicial:5, diaFinal:31, nacional:12, regional:0, local:0, centro:31}
-  ]
+  calendar:Array <calendario> = [
+    {nombre:"Septiembre", semana:['L', 'M', 'X', 'J', 'V', 'S','D'], dias:[], festivos:[], diaInicial:3, diaFinal:30, nacional:[], regional:[11], local:[29], centro:[]},
+    {nombre:"Octubre", semana:['L', 'M', 'X', 'J', 'V', 'S','D'], dias:[], festivos:[], diaInicial:5, diaFinal:31, nacional:[12], regional:[], local:[], centro:[31]}
+  ];
 
   public constructor(){
     
-    this.crearMes(this.septiembre);
-    this.crearMes(this.octubre);
+    this.crearMes(this.calendar);
 
   }
 
   crearMes(mes:Array<calendario>){
 
     let contador:number = 0;
+    let fiesta:boolean = false;
 
-    for(let i = 0; i < mes[0].diaFinal + contador; i++){
+    for(let i = 0; i < mes.length; i++){
 
-      if(i >= mes[0].diaInicial){ mes[0].dias[i] = i - (contador - 1); }
-      else {contador++;}
+      for(let j = 0; j < mes[i].diaFinal + contador; j++){
+
+        if(j >= mes[i].diaInicial){ 
+          
+          mes[i].dias[j] = (j - (contador - 1)); 
+          
+          for(let k = 0; k < mes[i].nacional.length; k++){ if(mes[i].dias[j] == mes[i].nacional[k]){ fiesta = true; } }
+
+          for(let k = 0; k < mes[i].regional.length; k++){ if(mes[i].dias[j] == mes[i].regional[k]){ fiesta = true; } }
+
+          for(let k = 0; k < mes[i].local.length; k++){ if(mes[i].dias[j] == mes[i].local[k]){ fiesta = true; } }
+
+          for(let k = 0; k < mes[i].centro.length; k++){ if(mes[i].dias[j] == mes[i].centro[k]){ fiesta = true; } }
+
+          if(fiesta){ mes[i].festivos[j] = true; fiesta = false; } 
+          else { mes[i].festivos[j] = false; } 
+
+        }
+        else {contador++;}
+        
+      }
+      contador = 0;
 
     }
-
   }
-
 }
