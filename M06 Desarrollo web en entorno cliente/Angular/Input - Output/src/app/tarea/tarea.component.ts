af76b8a46c6db7773ce59c6a92233c3773c7aeba
Arizaaaa <74736573+Arizaaaa@users.noreply.github.com>
2022-11-22 20:17:32 +0100

    TS (M06 - M09)

--- M06 Desarrollo web en entorno cliente/Angular/Input - Output/src/app/tarea/tarea.component.ts
+++ M06 Desarrollo web en entorno cliente/Angular/Input - Output/src/app/tarea/tarea.component.ts
@@ -8,6 +8,8 @@ import { Tarea } from '../models/tarea-model';
 })
 export class TareaComponent implements OnInit {
 
+  fecha = new Date("2019/01/16");
+
   @Input() tareas: Tarea;
 
   constructor() {
@@ -15,6 +17,25 @@ export class TareaComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  compararFecha(tareas:any){
+
+    let añoFin:number = parseInt(tareas.fechaFin.toString().substring(0, 4));
+    let mesFin:number = parseInt(tareas.fechaFin.toString().substring(5, 7));
+    let diaFin:number = parseInt(tareas.fechaFin.toString().substring(8, 10));
+
+    let año:number = parseInt(this.fecha.getFullYear().toString().substring(0, 4));
+    let mes:number = parseInt(this.fecha.getMonth().toString().substring(5, 7));
+    let dia:number = parseInt(this.fecha.getDay().toString().substring(8, 10));
+
+console.log(año, mes, dia);
+
+    if(año < añoFin && tareas.lista != "Finalizadas") {return "rojo"}
+    else if (año < añoFin && tareas.lista == "Finalizadas") {return "verde"}
+    else if ((dia - 1) >= diaFin && dia <= diaFin) {return "naranja"}
+    else {return "gris"}   
+
   }
 
 }
